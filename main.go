34e81e88d1b3c66c83a65cd652c3a7fa9c104cc8
Wabri <12409541+Wabri@users.noreply.github.com>
2023-01-08 20:53:45 +0100

    fix!: remove old and setup new baseline
    More to do on the next commits

--- main.go
+++ main.go
@@ -1,28 +1,4 @@
 package main
 
-import (
-	"os"
-
-	"github.com/Schrodinger-Hat/Daje/pkg/cmd/root"
-)
-
-type exitCode int
-
-const (
-	exitOk    exitCode = 0
-	exitError exitCode = 1
-)
-
 func main() {
-	code := mainRun()
-	os.Exit(int(code))
-}
-
-func mainRun() exitCode {
-	cmd := root.NewCmdRoot()
-	if _, err := cmd.ExecuteC(); err != nil {
-		return exitError
-	}
-
-	return exitOk
 }
